06c5dd933f1bbc513fb5a88b2bdb96bd2b6451c5
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-06-04 16:50:19 -0500

    agregando header

---
+++ index.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Sistema</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Empleados</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Cargos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Usuarios</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Cerrar Session</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <main class="container">
+        <br>
+        <h3>Este es main</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum praesentium repudiandae aliquam id commodi voluptatibus ipsum eos nemo ipsa perspiciatis minima enim, autem exercitationem at molestiae sint! Saepe, libero quasi.</p>
+    </main>
+
+    <footer>
+        llOrTeGall
+    </footer>
+
+    <!-- //* CDS Script para Boostrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</body>
+
+</html>
