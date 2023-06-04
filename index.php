f05cf3fc05f33f1045ec7881ba03ebd7f454c43e
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-06-04 16:54:23 -0500

    terminado los templates

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
+<!-- //* Aqui Esta El Header -->
 <?php include('templates/header.php'); ?>
 
 <br>
+
 <h3>Este es main</h3>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum praesentium repudiandae aliquam id commodi voluptatibus ipsum eos nemo ipsa perspiciatis minima enim, autem exercitationem at molestiae sint! Saepe, libero quasi.</p>
 
+<!-- //* Aqui Esta El Footer -->
 <?php include('templates/footer.php'); ?>
