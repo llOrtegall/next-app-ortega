58b979f97102c48d17e1e7bf4c0987a0e5749f62
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-06-04 16:53:11 -0500

    template creados

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php include('templates/header.php'); ?>
+
 <br>
 <h3>Este es main</h3>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum praesentium repudiandae aliquam id commodi voluptatibus ipsum eos nemo ipsa perspiciatis minima enim, autem exercitationem at molestiae sint! Saepe, libero quasi.</p>
+
+<?php include('templates/footer.php'); ?>
