048d9f90c662994b359c53a7b11614a8e6dd5c3e
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-06-06 17:07:55 -0500

    navegacion completa

---
+++ modules/cargos/editar.php
@@ -0,0 +1,7 @@
+<!-- //* Aqui Esta El Header -->
+<?php include('../../templates/header.php'); ?>
+
+<h2>Editar cargos</h2>
+
+<!-- //* Aqui Esta El Footer -->
+<?php include('../../templates/footer.php'); ?>
---
+++ modules/empleados/crear.php
@@ -0,0 +1,7 @@
+<!-- //* Aqui Esta El Header -->
+<?php include('../../templates/header.php'); ?>
+
+<h2>crear empleados</h2>
+
+<!-- //* Aqui Esta El Footer -->
+<?php include('../../templates/footer.php'); ?>
