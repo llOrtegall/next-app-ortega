3026a06ddc0a0377b000a16aad5a5ef9e255ce01
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-06-06 17:16:47 -0500

    agregando funcionalidad a empleados

--- modules/empleados/index.php
+++ modules/empleados/index.php
@@ -1,13 +1,37 @@
 <!-- //* Aqui Esta El Header -->
 <?php include('../../templates/header.php'); ?>
 
-<h2>listar empleados</h2>
+<br />
 
 <div class="card">
     <div class="card-header">
         Empleados
     </div>
     <div class="card-body">
+        <div class="table-responsive-sm">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Foto</th>
+                        <th scope="col">CV</th>
+                        <th scope="col">Cargo</th>
+                        <th scope="col">Fecha De Ingreso</th>
+                        <th scope="col">Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="">
+                        <td scope="row">Oscar Ivan Ortega</td>
+                        <td>imagen.jpg</td>
+                        <td>CV.pdf</td>
+                        <td>Programador</td>
+                        <td>2023/12/08</td>
+                        <td>Carta | Editar | Eliminar</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
 
     </div>
 </div>
