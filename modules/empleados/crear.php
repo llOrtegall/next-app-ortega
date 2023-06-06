db136a4cf37a47393ad0b010eccfa6c1dbab23bf
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-06-06 17:26:38 -0500

    forms creado

--- modules/empleados/crear.php
+++ modules/empleados/crear.php
@@ -1,7 +1,23 @@
 <!-- //* Aqui Esta El Header -->
 <?php include('../../templates/header.php'); ?>
 
-<h2>crear empleados</h2>
+<br />
+
+<div class="card">
+    <div class="card-header">
+        Datos Del Empleados
+    </div>
+    <div class="card-body">
+        <form action="" method="post" enctype="multipart/form-data">
+
+            <div class="mb-3">
+                <label" class="form-label">Primer Nombre</label>
+                    <input type="text" class="form-control" name="primernombre" id="primernombre" placeholder="Primer Apellido">
+            </div>
+
+        </form>
+    </div>
+</div>
 
 <!-- //* Aqui Esta El Footer -->
 <?php include('../../templates/footer.php'); ?>
