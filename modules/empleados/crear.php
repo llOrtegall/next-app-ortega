18fde9ba137bbd3e936227d2ffd7cab442b85d6e
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-06-06 17:39:14 -0500

    formulario terminado la maquetacion

--- modules/empleados/crear.php
+++ modules/empleados/crear.php
@@ -12,9 +12,51 @@
 
             <div class="mb-3">
                 <label" class="form-label">Primer Nombre</label>
-                    <input type="text" class="form-control" name="primernombre" id="primernombre" placeholder="Primer Apellido">
+                    <input type="text" class="form-control" name="primernombre" id="primernombre">
             </div>
 
+            <div class="mb-3">
+                <label for="segundonombre" class="form-label">Segundo Nombre</label>
+                <input type="text" class="form-control" name="segundonombre" id="segundonombre">
+            </div>
+
+            <div class="mb-3">
+                <label for="primerapellido" class="form-label">Primer Apellido</label>
+                <input type="text" class="form-control" name="primerapellido" id="primerapellido">
+            </div>
+
+            <div class="mb-3">
+                <label for="segundoapellido" class="form-label">Segundo Apellido</label>
+                <input type="text" class="form-control" name="segundoapellido" id="segundoapellido">
+            </div>
+
+            <div class="mb-3">
+                <label for="" class="form-label">Foto</label>
+                <input type="file" class="form-control" name="" id="">
+            </div>
+
+            <div class="mb-3">
+                <label for="" class="form-label">CV(PDF)</label>
+                <input type="file" class="form-control" name="" id="">
+            </div>
+
+            <div class="mb-3">
+                <label for="idpuesto" class="form-label">Puesto:</label>
+                <select class="form-select form-select-sm" name="idpuesto" id="idpuesto">
+                    <option selected>Select one</option>
+                    <option value="">New Delhi</option>
+                    <option value="">Istanbul</option>
+                    <option value="">Jakarta</option>
+                </select>
+            </div>
+
+            <div class="mb-3">
+                <label for="fechaingreso" class="form-label">Fecha De Ingreso</label>
+                <input type="date" class="form-control" name="fechaingreso" id="fechaingreso">
+            </div>
+
+            <button type="submit" class="btn btn-success">Agregar Registro</button>
+            <a name="" id="" class="btn btn-primary" href="index.php" role="button">Cancelar</a>
         </form>
     </div>
 </div>
