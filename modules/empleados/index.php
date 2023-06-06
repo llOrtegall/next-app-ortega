8922477c9a7e92410bf90fe6aedcadf3a52e7876
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-06-06 17:12:48 -0500

    agrengar table

--- modules/empleados/index.php
+++ modules/empleados/index.php
@@ -3,5 +3,15 @@
 
 <h2>listar empleados</h2>
 
+<div class="card">
+    <div class="card-header">
+        Empleados
+    </div>
+    <div class="card-body">
+
+    </div>
+</div>
+
+
 <!-- //* Aqui Esta El Footer -->
 <?php include('../../templates/footer.php'); ?>
