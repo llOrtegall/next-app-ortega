0d04896726f5bcd1d71995809031839ab242d54e
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-06-07 09:31:31 -0500

    cargos listo sigue usuarios

--- modules/cargos/crear.php
+++ modules/cargos/crear.php
@@ -1,7 +1,28 @@
 <!-- //* Aqui Esta El Header -->
 <?php include('../../templates/header.php'); ?>
 
-crear cargos
+<br />
+
+<div class="card">
+    <div class="card-header">
+        Cargos
+    </div>
+    <div class="card-body">
+
+        <form action="" method="post" enctype="multipart/form-data">
+            <div class="mb-3">
+                <label for="nombreCargo" class="form-label">Nombre Del Puesto</label>
+                <input type="text" class="form-control" name="nombreCargo" id="nombreCargo" placeholder="cargo">
+            </div>
+
+            <button type="submit" class="btn btn-success">Agregar</button>
+            <a name="" id="" class="btn btn-primary" href="index.php" role="button">Cancelar</a>
+        </form>
+    </div>
+
+
+
+</div>
 
 <!-- //* Aqui Esta El Footer -->
 <?php include('../../templates/footer.php'); ?>
