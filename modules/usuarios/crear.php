57269d809d7f3d78824aa6b4f980bb73820834a9
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-06-09 19:57:36 -0500

    agregar usuario ok

--- modules/usuarios/crear.php
+++ modules/usuarios/crear.php
@@ -1,3 +1,25 @@
+<?php
+include("../../db.php");
+if ($_POST) {
+  print_r($_POST);
+
+  //*Recolectamos los nombres
+  $usuario = (isset($_POST["usuario"]) ? $_POST["usuario"] : "");
+  $password = (isset($_POST["password"]) ? $_POST["password"] : "");
+  $correo = (isset($_POST["correo"]) ? $_POST["correo"] : "");
+
+  //* prepara para insertar registros
+  $sentencia = $conexcion->prepare("INSERT INTO tbl_usuarios(id, usuario, password, email) VALUES(null, :usuario, :password, :correo)");
+
+  //* Asigna que tienen use de :variable
+  $sentencia->bindParam(":usuario", $usuario);
+  $sentencia->bindParam(":password", $password);
+  $sentencia->bindParam(":correo", $correo);
+  $sentencia->execute();
+}
+
+?>
+
 <!-- //* Aqui Esta El Header -->
 <?php include('../../templates/header.php'); ?>
 
@@ -14,12 +36,12 @@
         <input type="text" class="form-control" name="usuario" id="usuario" placeholder="usuario">
       </div>
       <div class="mb-3">
-        <label for="" class="form-label">Password</label>
-        <input type="password" class="form-control" name="" id="" placeholder="*********">
+        <label for="password" class="form-label">Password</label>
+        <input type="password" class="form-control" name="password" id="password" placeholder="*********">
       </div>
       <div class="mb-3">
-        <label for="" class="form-label">Correo:</label>
-        <input type="email" class="form-control" name="" id="" placeholder="correo@correo.com">
+        <label for="correo" class="form-label">Correo:</label>
+        <input type="email" class="form-control" name="correo" id="correo" placeholder="correo@correo.com">
       </div>
       <button type="submit" class="btn btn-success">Agregar</button>
       <a name="" id="" class="btn btn-primary" href="index.php" role="button">Cancelar</a>
