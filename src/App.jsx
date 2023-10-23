81f2b712253e82ac6837ad10a1ad7a321e572d1d
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-10-23 08:00:45 -0500

    funciona

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import { Home } from './pages/Home.jsx'
+import { AboutUs } from './pages/AboutUs.jsx'
+import { Route, Routes } from 'react-router-dom'
+
 export function App () {
   return (
-    <>
-      <h2>Hello World</h2>
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutUs />} />
+      </Routes>
   )
 }
