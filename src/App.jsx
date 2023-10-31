c083aec86384a5defbff628a5acd15c32e900647
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-10-31 08:07:27 -0500

    implemetando la navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import { Home } from './pages/Home.jsx'
 import { AboutUs } from './pages/AboutUs.jsx'
 import { Route, Routes } from 'react-router-dom'
 import { Products } from './pages/Prodcuts.jsx'
+import { NavBar } from './components/NavBar.jsx'
 
 export function App () {
   return (
+    <>
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<AboutUs />} />
         <Route path="/products" element={<Products />} />
       </Routes>
+    </>
   )
 }
