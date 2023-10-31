8037a5a2815b3455f86fd9aa02494171c5cfeba3
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-10-31 08:12:23 -0500

    agregando layout

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,18 +2,23 @@ import { Link } from 'react-router-dom'
 
 export function NavBar () {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to='/'> Home </Link>
-        </li>
-        <li>
-          <Link to='about'> About Us </Link>
-        </li>
-        <li>
-          <Link to='products'> Products </Link>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <h1>Welcome To React Router</h1>
+      <hr />
+      <nav>
+        <ul style={{ display: 'flex', justifyContent: 'space-around' }}>
+          <li>
+            <Link to='/'> Home </Link>
+          </li>
+          <li>
+            <Link to='about'> About Us </Link>
+          </li>
+          <li>
+            <Link to='products'> Products </Link>
+          </li>
+        </ul>
+      </nav>
+      <hr />
+    </>
   )
 }
