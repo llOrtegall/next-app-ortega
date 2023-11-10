b713ed40da7c94b5b55607c6e2535fc3b886be48
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-11-10 08:21:09 -0500

    Oulet implementado

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { Layout } from './components/Layout.jsx'
 import { Route, Routes } from 'react-router-dom'
 
-// import { AboutUs } from './pages/AboutUs.jsx'
-// import { Products } from './pages/Prodcuts.jsx'
-// import { Home } from './pages/Home.jsx'
+import { AboutUs } from './pages/AboutUs.jsx'
+import { Products } from './pages/Prodcuts.jsx'
+import { Home } from './pages/Home.jsx'
 
 export function App () {
   return (
     <Routes>
-      <Route path="/" element={<Layout />} />
+      <Route path="/" element={<Layout />} >
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/products" element={<Products />} />
+      </Route>
       {/* <Route path="/" element={<Home />} />
       <Route path="/about" element={<AboutUs />} />
       <Route path="/products" element={<Products />} /> */}
